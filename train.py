9395dfa26e6e17aa02b2c1328792119f299327d2
KYM384 <daiki.mamekku@hotmail.com>
2026-03-17 12:36:44 +0900

    init commit

--- train.py
+++ train.py
@@ -12,6 +12,7 @@
 
 import os
 import time
+import wandb
 import random
 import argparse
 import numpy as np
@@ -43,9 +44,6 @@ class TrainOptions:
 
         # input/output sizes       
         self.parser.add_argument('--batchSize', type=int, default=4, help='input batch size')       
-
-        # for displays
-        self.parser.add_argument('--use_tensorboard', type=str2bool, default='False')
 
         # for training
         self.parser.add_argument('--dataset', type=str, default="/path/to/VGGFace2", help='path to the face swapping dataset')
@@ -132,6 +130,8 @@ if __name__ == '__main__':
     
     cudnn.benchmark = True
 
+    wandb.init(project="DriftGAN", name="SimSwap pretrain", config=opt)
+
     
 
     model = fsModel()
@@ -139,10 +139,7 @@ if __name__ == '__main__':
     model.initialize(opt)
 
     #####################################################
-    if opt.use_tensorboard:
-        tensorboard_writer  = tensorboard.SummaryWriter(log_path)
-        logger              = tensorboard_writer
-        
+
     log_name = os.path.join(opt.checkpoints_dir, opt.name, 'loss_log.txt')
 
     with open(log_name, "a") as log_file:
@@ -175,58 +172,59 @@ if __name__ == '__main__':
 
     # Training Cycle
     for step in range(start, total_step):
-        model.netG.train()
-        for interval in range(2):
-            random.shuffle(randindex)
-            src_image1, src_image2  = train_loader.next()
-            
-            if step%2 == 0:
-                img_id = src_image2
-            else:
-                img_id = src_image2[randindex]
-
-            img_id_112      = F.interpolate(img_id,size=(112,112), mode='bicubic')
-            latent_id       = model.netArc(img_id_112)
-            latent_id       = F.normalize(latent_id, p=2, dim=1)
-            if interval:
+        with torch.autocast("cuda", torch.bfloat16):
+            model.netG.train()
+            for interval in range(2):
+                random.shuffle(randindex)
+                src_image1, src_image2  = train_loader.next()
                 
-                img_fake        = model.netG(src_image1, latent_id)
-                gen_logits,_    = model.netD(img_fake.detach(), None)
-                loss_Dgen       = (F.relu(torch.ones_like(gen_logits) + gen_logits)).mean()
-
-                real_logits,_   = model.netD(src_image2,None)
-                loss_Dreal      = (F.relu(torch.ones_like(real_logits) - real_logits)).mean()
-
-                loss_D          = loss_Dgen + loss_Dreal
-                optimizer_D.zero_grad()
-                loss_D.backward()
-                optimizer_D.step()
-            else:
-                
-                # model.netD.requires_grad_(True)
-                img_fake        = model.netG(src_image1, latent_id)
-                # G loss
-                gen_logits,feat = model.netD(img_fake, None)
-                
-                loss_Gmain      = (-gen_logits).mean()
-                img_fake_down   = F.interpolate(img_fake, size=(112,112), mode='bicubic')
-                latent_fake     = model.netArc(img_fake_down)
-                latent_fake     = F.normalize(latent_fake, p=2, dim=1)
-                loss_G_ID       = (1 - model.cosin_metric(latent_fake, latent_id)).mean()
-                real_feat       = model.netD.get_feature(src_image1)
-                feat_match_loss = model.criterionFeat(feat["3"],real_feat["3"]) 
-                loss_G          = loss_Gmain + loss_G_ID * opt.lambda_id + feat_match_loss * opt.lambda_feat
-                
-
                 if step%2 == 0:
-                    #G_Rec
-                    loss_G_Rec  = model.criterionRec(img_fake, src_image1) * opt.lambda_rec
-                    loss_G      += loss_G_Rec
+                    img_id = src_image2
+                else:
+                    img_id = src_image2[randindex]
 
-                optimizer_G.zero_grad()
-                loss_G.backward()
-                optimizer_G.step()
-                
+                img_id_112      = F.interpolate(img_id,size=(112,112), mode='bicubic')
+                latent_id       = model.netArc(img_id_112)
+                latent_id       = F.normalize(latent_id, p=2, dim=1)
+                if interval:
+                    
+                    img_fake        = model.netG(src_image1, latent_id)
+                    gen_logits,_    = model.netD(img_fake.detach(), None)
+                    loss_Dgen       = (F.relu(torch.ones_like(gen_logits) + gen_logits)).mean()
+
+                    real_logits,_   = model.netD(src_image2,None)
+                    loss_Dreal      = (F.relu(torch.ones_like(real_logits) - real_logits)).mean()
+
+                    loss_D          = loss_Dgen + loss_Dreal
+                    optimizer_D.zero_grad()
+                    loss_D.backward()
+                    optimizer_D.step()
+                else:
+                    
+                    # model.netD.requires_grad_(True)
+                    img_fake        = model.netG(src_image1, latent_id)
+                    # G loss
+                    gen_logits,feat = model.netD(img_fake, None)
+                    
+                    loss_Gmain      = (-gen_logits).mean()
+                    img_fake_down   = F.interpolate(img_fake, size=(112,112), mode='bicubic')
+                    latent_fake     = model.netArc(img_fake_down)
+                    latent_fake     = F.normalize(latent_fake, p=2, dim=1)
+                    loss_G_ID       = (1 - model.cosin_metric(latent_fake, latent_id)).mean()
+                    real_feat       = model.netD.get_feature(src_image1)
+                    feat_match_loss = model.criterionFeat(feat["3"],real_feat["3"]) 
+                    loss_G          = loss_Gmain + loss_G_ID * opt.lambda_id + feat_match_loss * opt.lambda_feat
+                    
+
+                    if step%2 == 0:
+                        #G_Rec
+                        loss_G_Rec  = model.criterionRec(img_fake, src_image1) * opt.lambda_rec
+                        loss_G      += loss_G_Rec
+
+                    optimizer_G.zero_grad()
+                    loss_G.backward()
+                    optimizer_G.step()
+                    
 
         ############## Display results and errors ##########
         ### print out errors
@@ -242,9 +240,8 @@ if __name__ == '__main__':
                 "D_real":loss_Dreal.item(),
                 "D_loss":loss_D.item()
             }
-            if opt.use_tensorboard:
-                for tag, value in errors.items():
-                    logger.add_scalar(tag, value, step)
+            wandb.log(errors, step=step+1)
+
             message = '( step: %d, ) ' % (step)
             for k, v in errors.items():
                 message += '%s: %.3f ' % (k, v)
@@ -282,9 +279,12 @@ if __name__ == '__main__':
                 imgs = np.stack(imgs, axis = 0).transpose(0,2,3,1)
                 plot_batch(imgs, os.path.join(sample_path, 'step_'+str(step+1)+'.jpg'))
 
+                wandb.log({"samples":[wandb.Image(os.path.join(sample_path, 'step_'+str(step+1)+'.jpg'))]}, step=step+1)
+
         ### save latest model
         if (step+1) % opt.model_freq==0:
             print('saving the latest model (steps %d)' % (step+1))
             model.save(step+1)            
             np.savetxt(iter_path, (step+1, total_step), delimiter=',', fmt='%d')
+
     wandb.finish()
